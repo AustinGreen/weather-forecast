7e3c3cbdeb00c79142f7a8ce43d5d33356cba87f
Austin Green <austinzgreen@gmail.com>
2017-04-23 22:49:58 -0400

    icons

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -7,46 +7,8 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import Footer from '../../shared/Footer';
 import { SkyLandscape } from '../../shared/Sky';
+import icons from '../../shared/Icons';
 import TransparentBackground from '../../shared/Backgrounds';
-import icon01d from '../../img/weather-icons/01d.svg';
-import icon01n from '../../img/weather-icons/01n.svg';
-import icon02d from '../../img/weather-icons/02d.svg';
-import icon02n from '../../img/weather-icons/02n.svg';
-import icon03d from '../../img/weather-icons/03d.svg';
-import icon03n from '../../img/weather-icons/03n.svg';
-import icon04d from '../../img/weather-icons/04d.svg';
-import icon04n from '../../img/weather-icons/04n.svg';
-import icon09d from '../../img/weather-icons/09d.svg';
-import icon09n from '../../img/weather-icons/09n.svg';
-import icon10d from '../../img/weather-icons/10d.svg';
-import icon10n from '../../img/weather-icons/10n.svg';
-import icon11d from '../../img/weather-icons/11d.svg';
-import icon11n from '../../img/weather-icons/11n.svg';
-import icon13d from '../../img/weather-icons/13d.svg';
-import icon13n from '../../img/weather-icons/13n.svg';
-import icon50d from '../../img/weather-icons/50d.svg';
-import icon50n from '../../img/weather-icons/50n.svg';
-
-const icons = {
-  icon01d,
-  icon01n,
-  icon02d,
-  icon02n,
-  icon03d,
-  icon03n,
-  icon04d,
-  icon04n,
-  icon09d,
-  icon09n,
-  icon10d,
-  icon10n,
-  icon11d,
-  icon11n,
-  icon13d,
-  icon13n,
-  icon50d,
-  icon50n,
-};
 
 const Loader = styled.a`
   font-size: 3rem;
@@ -62,6 +24,7 @@ const ImageContainer = styled.figure`
 
 const Card = styled.div`
   transition: all 0.2s ease-in-out;
+  background-color: rgba(255,255,255,0.7);
   &:hover {
     box-shadow: 0 4px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
     transform: scale(1.1);
---
+++ src/shared/Icons.js
@@ -0,0 +1,41 @@
+import icon01d from '../img/weather-icons/01d.svg';
+import icon01n from '../img/weather-icons/01n.svg';
+import icon02d from '../img/weather-icons/02d.svg';
+import icon02n from '../img/weather-icons/02n.svg';
+import icon03d from '../img/weather-icons/03d.svg';
+import icon03n from '../img/weather-icons/03n.svg';
+import icon04d from '../img/weather-icons/04d.svg';
+import icon04n from '../img/weather-icons/04n.svg';
+import icon09d from '../img/weather-icons/09d.svg';
+import icon09n from '../img/weather-icons/09n.svg';
+import icon10d from '../img/weather-icons/10d.svg';
+import icon10n from '../img/weather-icons/10n.svg';
+import icon11d from '../img/weather-icons/11d.svg';
+import icon11n from '../img/weather-icons/11n.svg';
+import icon13d from '../img/weather-icons/13d.svg';
+import icon13n from '../img/weather-icons/13n.svg';
+import icon50d from '../img/weather-icons/50d.svg';
+import icon50n from '../img/weather-icons/50n.svg';
+
+const icons = {
+  icon01d,
+  icon01n,
+  icon02d,
+  icon02n,
+  icon03d,
+  icon03n,
+  icon04d,
+  icon04n,
+  icon09d,
+  icon09n,
+  icon10d,
+  icon10n,
+  icon11d,
+  icon11n,
+  icon13d,
+  icon13n,
+  icon50d,
+  icon50n,
+};
+
+export default icons;
